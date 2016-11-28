4457c3a6e1cc15e44e8df63debbc20af532d941a
Sang Young Oh <sangyoung@gmail.com>
2016-11-28 20:41:19 +0900

    Delete unused files

--- types/xxx/sorted-array-map.d.ts
+++
@@ -1,9 +0,0 @@
-declare class SortedArrayMaps {
-    constructor(values?: any, equals?: any, compare?: any, getDefault?: any);
-}
-
-interface SortedArrayMaps {
-    (values?: any, equals?: any, compare?: any, getDefault?: any): SortedArrayMaps
-}
-
-export = SortedArrayMaps;
